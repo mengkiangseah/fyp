b182a81f10c3e3989500ec448f906a7c7d8a2d79
The Project <project@noreply.raspbx.org>
2017-05-29 17:01:23 +0100

    Working polling for changes system. Note that the change happens during the call, not after, which is good.

--- code/test_change.py
+++ code/test_change.py
@@ -4,23 +4,36 @@ import os
 import time
 
 # Main file
+def directory_listing(path_to_watch):
+	the_output = []
+	
+	for root, dirs, files in os.walk(path_to_watch, topdown=False):
+		for name in files:
+			the_output.append(os.path.join(root, name))
+		for name in dirs: 
+			the_output.append(os.path.join(root, name))
+	return the_output
+
 def main():
-    # Define path to recordings
-    path_to_watch = "/var/spool/asterisk/monitor"
+	# Define path to recordings
+	path_to_watch = "/var/spool/asterisk/monitor"
 
-    # Baseline
-    before = dict ([(f, None) for f in os.walk (path_to_watch)])
-    print(before)
+	# Baseline
+	before = directory_listing(path_to_watch)
+	print(before)
 
-    # Polling loop
-    while 1:
-        time.sleep (10)
-        after = dict ([(f, None) for f in os.walk (path_to_watch)])
-        added = [f for f in after if not f in before]
-        if added:
-            print("Added: ")
-            print(added)
-        before = after
+	# Polling loop
+	while 1:
+		time.sleep (10)
+		after = directory_listing(path_to_watch)
+	
+		added = [f for f in after if (not f in before and ".wav" in f)]
+	
+		if added:
+			print("Added: ")
+			print(added)
+
+		before = after
 
 if __name__ == '__main__':
-    main()
+	main()
