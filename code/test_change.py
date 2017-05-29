ac6a2070cea59ab3d291e974c9cdd710c04ecd32
Meng Kiang Seah <mengkiang92@gmail.com>
2017-05-29 16:23:37 +0100

    Initial test.

--- code/test_change.py
+++ code/test_change.py
@@ -1,1 +1,25 @@
-Nothing atm.
+#!/usr/bin/python3
+
+import os, time
+
+# Main file
+def main():
+    # Define path to recordings
+    path_to_watch = "/var/spool/asterisk/monitor"
+
+    # Baseline
+    before = dict ([(f, None) for f in os.listdir (path_to_watch)])
+    print(before)
+
+    # Polling loop
+    while 1:
+      time.sleep (10)
+      after = dict ([(f, None) for f in os.listdir (path_to_watch)])
+      added = [f for f in after if not f in before]
+      if added:
+          print("Added: ")
+          print(added)
+      before = after
+
+ if __name__ == '__main__':
+     main()
