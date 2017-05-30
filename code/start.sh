ad96d78c827873efdd0cfc86bf6fcb8f4bbb84e4
The Project <project@noreply.raspbx.org>
2017-05-30 20:42:14 +0100

    Changes to the startup script.

--- code/start.sh
+++ code/start.sh
@@ -2,16 +2,11 @@
 
 # Startup script for kiosk mode
 
-echo "Script running..."
+sudo startx /usr/bin/chromium-browser --kiosk --no-sandbox --window-size=1280,1024 --window-position=0,0 --disable-session-crashed-bubble --disable-infobars file:///
 
 xset s off
 xset -dpms
 xset s noblank
 
-echo "Screensavers and screen blanking removed."
-
 unclutter &
 
-echo "Unclutter to remove mouse pointer done."
-
-startx /usr/bin/chromium-browser --kiosk --no-sandbox -- window-size=1280,1024 --window-position=0,0 --disable-session-crashed-bubble --disable-infobars file:/// &
