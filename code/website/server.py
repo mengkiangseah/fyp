5e79156cb2bf7b742510e1f4e59d6c2d7ac94da7
Meng Kiang Seah <mengkiang92@gmail.com>
2017-06-01 19:54:50 +0100

    Working stuff. Server needs monitoring software.

--- code/website/server.py
+++ code/website/server.py
@@ -1,11 +1,18 @@
 from flask import Flask, render_template
-import random
 app = Flask(__name__)
+
+global state
 
 @app.route("/")
 def index():
-    state = random.randint(1, 8)
+    global state
+    state = (state + 1) % 8
     return render_template('index.html', the_state=state)
 
+@app.route("/static/jquery.js")
+def jquery():
+    return app.send_static_file('jquery.js')
+
 if __name__ == "__main__":
+    state = 0
     app.run()
