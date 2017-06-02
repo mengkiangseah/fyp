ee9f2962a8eefc869b39df4706a47d38a8e6777c
Meng Kiang Seah <mengkiang92@gmail.com>
2017-06-02 18:31:08 +0100

    New start.sh script. New testing running.

--- code/start.sh
+++ code/start.sh
@@ -1,10 +1,14 @@
 # !/bin/bash
 
+# Launch monitoring server
+
+/home/project/fyp/code/test_multithreading_server.py &
+
 # Startup script for kiosk mode
 
 export DISPLAY=:0
 
-sudo startx /usr/bin/chromium-browser --kiosk --no-sandbox --window-size=5000,5000 --window-position=0,0 --disable-session-crashed-bubble --disable-infobars http://127.0.0.1:5000 &  
+sudo startx /usr/bin/chromium-browser --kiosk --no-sandbox --window-size=5000,5000 --window-position=0,0 --disable-session-crashed-bubble --disable-infobars http://127.0.0.1:5000 &
 
 echo "Done."
 echo "Waiting."
---
+++ code/test_multithreading_server.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template
+import threading
+import time
+
+app = Flask(__name__)
+
+global state
+state = 0
+
+@app.route("/")
+def index():
+    global state
+    return render_template('index.html', the_state=state)
+
+def serverFunction():
+    app.run()
+
+def incrementFunction():
+    global state
+    while(True):
+        state = state + 1
+        time.sleep(7)
+
+
+if __name__ == "__main__":
+    incrementThread = threading.Thread(target=incrementFunction)
+    serverThread = threading.Thread(target=serverFunction)
+
+    print("Running incrementThread.")
+    incrementThread.start();
+    print("Running serverThread.")
+    serverThread.start();
