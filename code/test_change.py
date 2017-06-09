f50a3d309f39357b5137949331d5f9db657cac0d
Meng Kiang Seah <mengkiang92@gmail.com>
2017-06-09 21:02:39 +0100

    Some experimental changes.

--- code/test_change.py
+++ code/test_change.py
@@ -2,15 +2,16 @@
 
 import os
 import time
+import wave
 
 # Main file
 def directory_listing(path_to_watch):
 	the_output = []
-	
+
 	for root, dirs, files in os.walk(path_to_watch, topdown=False):
 		for name in files:
 			the_output.append(os.path.join(root, name))
-		for name in dirs: 
+		for name in dirs:
 			the_output.append(os.path.join(root, name))
 	return the_output
 
@@ -23,15 +24,28 @@ def main():
 	print(before)
 
 	# Polling loop
-	while 1:
+	while True:
 		time.sleep (10)
 		after = directory_listing(path_to_watch)
-	
+
 		added = [f for f in after if (not f in before and ".wav" in f)]
-	
+
+		# New file found, call is therefore incoming.
 		if added:
-			print("Added: ")
-			print(added)
+			# Open recording
+			print(added[0])
+			audioFileIn = wave.open(addedFiles[0], mode='rb')
+
+			oldsizeData = audioFileIn.getnframes()
+
+			# Wait for data to collect
+			time.sleep(lengthAudio + 0.5)
+
+			# Prepare file to be sent. Open output file, select data from recording, write.
+			audioFileOut = wave.open("/home/pi/output.wav", mode= 'wb')
+			audioFileOut.setparams((1, 2, 8000, 8000 * lengthAudio, 'NONE', 'not compressed'))
+			audioFileIn.readframes(oldsizeData)
+			audioFileOut.writeframes(audioFileIn.readframes(8000 * lengthAudio))
 
 		before = after
 
