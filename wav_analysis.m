00170910de22c0f55635109a8078985e3fcddd84
Meng Kiang Seah <mengkiang92@gmail.com>
2017-06-20 05:07:02 +0100

    Lost of writing lol.

--- wav_analysis.m
+++ wav_analysis.m
@@ -1,15 +1,26 @@
-filename = 'recording3.wav';
+filename = 'recording2.wav';
 
 [y,samplingfreq] = audioread(filename);
 information = audioinfo(filename);
 
 timing = linspace(0, information.Duration, information.TotalSamples);
-
+%%
+figure('position', [0 0 1280 800]);
 plot(timing, y);
 axis([0, information.Duration, min(y), max(y)]);
+xlabel('Time/s')
+ylabel('Value')
+set(findall(gcf,'type','axes'),'fontsize',50);
+set(findall(gcf,'type','text'),'fontSize',50);
 
 %%
 begin = 1.5e4;
 finish = 1.6e4;
 
-plot(y(1:end));
+figure('position', [0 0 1280 800]);
+plot(timing, y);
+axis([1.2, 1.6, min(y), max(y)]);
+xlabel('Time/s')
+ylabel('Value')
+set(findall(gcf,'type','axes'),'fontsize',50);
+set(findall(gcf,'type','text'),'fontSize',50);
